0bab3dd9edb4a3cb2f15de1bf84ca14bc4122e94
Lucas Ergh <lucasergh@gmail.com>
2025-01-22 14:49:57 -0500

    Add services and an endpoint for the SignalR hub

--- ConnectFour/Hubs/ConnectFourHub.cs
+++ ConnectFour/Hubs/ConnectFourHub.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace ConnectFour.Hubs;
 
-public class ChatHub : Hub {
+public class ConnectFourHub : Hub {
     public async Task SendMessage(string GameRoom, string Message){
         await Clients.All.SendAsync("ReceiveMessage", GameRoom, Message);
     }
--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,5 +1,7 @@
 using ConnectFour.Client.Pages;
 using ConnectFour.Components;
+using ConnectFour.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,16 @@ builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
+app.UseResponseCompression();
+
+// SignalR and Response Compression Middleware services
+builder.Services.AddSignalR();
+
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        [ "application/octet-stream"]);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,4 +43,5 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(ConnectFour.Client._Imports).Assembly);
 
+app.MapHub<ConnectFourHub>("/connectfour");
 app.Run();
